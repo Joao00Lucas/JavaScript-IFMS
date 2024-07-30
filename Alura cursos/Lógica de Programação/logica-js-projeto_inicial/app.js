8db2e6f9a3abec8401999ccddc1b206f47ba6dc0
Joao Lucas <joaolucas3230garcia@gmail.com>
2024-07-30 09:17:07 -0400

    retomando

--- Alura cursos/Lógica de Programação/logica-js-projeto_inicial/app.js
+++ Alura cursos/Lógica de Programação/logica-js-projeto_inicial/app.js
@@ -1,11 +1,21 @@
 alert('Boas vindas ao jogo número secreto');
-let numeroSecreto = 4;
+let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 1;
 
-// se chute for igual ao número secreto
-if (numeroSecreto == chute) {
-alert(`isso ai você descobriu o numero secreto ${numeroSecreto}`);
-} else {
-    alert('voce errou :(');
+while(chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    // se chute for igual ao número secreto
+    if (numeroSecreto == chute) {
+        alert(`isso ai você descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if ( chute > numeroSecreto) {
+            alert(`O numero secreto é menor que ${chute}`);
+        } else {
+            alert(`O numero secreto é maior que ${chute}`);
+        }
+    }
+
+    tentativas ++;
 }
